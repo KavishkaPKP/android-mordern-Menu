19fb619f838ccea494256265e5437b606a1b6cb7
Pasindu Kavishka <IT19123332@my.sliit.lk>
2021-09-05 14:43:51 +0530

    new menu

--- app/src/main/java/com/example/mmanu2/MainActivity.java
+++ app/src/main/java/com/example/mmanu2/MainActivity.java
@@ -11,7 +11,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //action bar hide onli this screen
+        //action bar hide only this screen
         getSupportActionBar().hide();
+
+
+
     }
 }
